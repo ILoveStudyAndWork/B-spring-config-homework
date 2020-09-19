a44ab42b72c52d621e8452e24f5830849036a769
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-09-19 22:06:28 +0800

    [fengyi_ling] refactor:delete un-use line

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelController.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelController.java
@@ -1,17 +1,10 @@
 package com.thoughtworks.capability.gtb.demospringconfig.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
-import org.springframework.core.env.MapPropertySource;
-import org.springframework.core.env.MutablePropertySources;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.Map;
 import java.util.Objects;
 
 @RestController
--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTest.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.capability.gtb.demospringconfig.controller;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -29,11 +28,10 @@ class LevelControllerTest {
     private ConfigurableEnvironment configEnv;
 
     @AfterEach
-    void tearDown() throws Exception {
+    void tearDown(){
         MutablePropertySources propertySources = configEnv.getPropertySources();
         Map<String, Object> map = Collections.singletonMap("levelNumber", 0);
         propertySources.addFirst(new MapPropertySource("new", map));
-
     }
 
     @Test
