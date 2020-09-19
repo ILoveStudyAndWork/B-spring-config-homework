f79a22a4349c922e2525e56b932b3990608881b1
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-09-19 19:20:59 +0800

    [fengyi_ling] feat:use setter to complete problem2

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/controller/LevelControllerTest.java
@@ -1,10 +1,11 @@
 package com.thoughtworks.capability.gtb.demospringconfig.controller;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -19,24 +20,26 @@ class LevelControllerTest {
     MockMvc mockMvc;
 
     @Autowired
-    LevelController levelController;
+    private LevelController levelController;
+
+
     @Test
     void shouldReturnBasicWhenGetLevelGivenLevelLessThan1() throws Exception {
-        ReflectionTestUtils.setField(levelController, "levelNumber", 0);
+        levelController.setLevelNumber(0);
         mockMvc.perform(get("/level"))
                 .andExpect(jsonPath("$", is("basic")));
     }
 
     @Test
     void shouldReturnBasicWhenGetLevelGivenLevelEquals1() throws Exception {
-        ReflectionTestUtils.setField(levelController, "levelNumber", 1);
+        levelController.setLevelNumber(1);
         mockMvc.perform(get("/level"))
                 .andExpect(jsonPath("$", is("advanced")));
     }
 
     @Test
     void shouldReturnAdvancedWhenGetLevelGivenLevelGreaterThan1() throws Exception {
-        ReflectionTestUtils.setField(levelController, "levelNumber", 2);
+        levelController.setLevelNumber(2);
         mockMvc.perform(get("/level"))
                 .andExpect(jsonPath("$", is("advanced")));
     }
